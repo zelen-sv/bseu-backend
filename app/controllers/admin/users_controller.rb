b510b8698a4259aba53b5025f7bd13106f966fde
kyalex <st.zelenko@gmail.com>
2018-10-30 15:09:08 +0300

    admin panel for users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,44 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:edit, :update, :destroy]
+
+  def new
+    @user = User.new
+  end
+
   def index
     @users = User.all
+  end
+
+  def edit
+  end
+
+  def update
+    return redirect_to admin_users_path if @user.update(user_params)
+    flash.now[:error] = 'Не удалось обновить пользователя!'
+    render 'edit'
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    return redirect_to admin_users_path if @user.save
+    flash.now[:error] = 'Не удалось создать пользователя!'
+    render 'new'
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
