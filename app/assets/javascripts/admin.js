50bcf6f61c3460c7602e31ff032a84d626b6ed0d
zelen-sv <st.zelenko@gmail.com>
2018-11-20 12:38:37 +0300

    admin specialties search

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,5 +1,6 @@
 function enableSpecialtyFilter () {
   var params = {
+    specialty_title: $("select#specialties").val(),
     faculty_id: $("select#faculties").val(),
     education_program: $("select#education_program").val(),
     education_form: $("select#education_form").val()
