cdc3b736c2af1da98cdb436d30617df22f0feaf1
zelen-sv <st.zelenko@gmail.com>
2018-12-02 19:28:09 +0300

    fix: redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,10 @@
+uri = "redis://localhost:6379/0"
+app_name = File.basename(Rails.root.to_s)
+
 Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://localhost:6379/0', namespace: "bseu_#{Rails.env}" }
+ config.redis = { url: uri, namespace: "#{app_name}_#{Rails.env}" }
+end
+
+Sidekiq.configure_client do |config|
+ config.redis = { url: uri, namespace: "#{app_name}_#{Rails.env}" }
 end
